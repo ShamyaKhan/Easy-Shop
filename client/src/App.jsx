921e55cb80bfa52ea4748c37fb574bbeb0d41e5f
Shamya Khan <md.shamiulhaque127@gmail.com>
2026-03-09 17:29:13 +0600

    small change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,6 @@ function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       {/* common components */}
-      <h1>Header Component</h1>
       <Routes>
         <Route
           path="/"
